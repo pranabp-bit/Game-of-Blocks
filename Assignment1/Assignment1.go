0227a6aaf046066c44ad6114f8a075dbf8e94a0e
pranabp-bit <58358551+pranabp-bit@users.noreply.github.com>
2021-05-23 18:12:43 +0530

    Update Assignment1.go

--- Assignment1/Assignment1.go
+++ Assignment1/Assignment1.go
@@ -40,3 +40,24 @@ func main() {
 	}
 
 }
+/*Sample Output
+
+abc
+Input is abc
+x is 767150
+Hence combination is abc767150
+Time elapsed: 285.6863ms
+
+assassin
+Input is assassin
+x is 1198235
+Hence combination is assassin1198235
+Time elapsed: 469.4712ms
+
+qwerty
+Input is qwerty
+x is 1597159
+Hence combination is qwerty1597159
+Time elapsed: 617.9101ms
+
+*/
